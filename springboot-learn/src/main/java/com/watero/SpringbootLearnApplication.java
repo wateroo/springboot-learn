cef73d2a1482b72adddc028b80c88e57b5140c57
yang <897238156@qq.com>
2020-09-19 20:04:08 +0800

    1

--- springboot-learn/src/main/java/com/watero/SpringbootLearnApplication.java
+++ springboot-learn/src/main/java/com/watero/SpringbootLearnApplication.java
@@ -2,12 +2,16 @@ package com.watero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootLearnApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootLearnApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringbootLearnApplication.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String s : beanDefinitionNames) {
+            System.out.println(s);
+        }
+    }
 }
